3631d7eef004c0c382bce2b4612599f87487a77f
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-03-17 20:00:40 +0700

    Update MPoligon.java

--- Praktikum 4/Tugas Standar/org/main/MPoligon.java
+++ Praktikum 4/Tugas Standar/org/main/MPoligon.java
@@ -1,3 +1,8 @@
+/**
+ * File :MPoligon.java 16/03/2024
+ * Penulis :Jeremia Joel Richard Ramalael
+ * Deskripsi: source code file main untuk memanggil bangundatar
+ */
 package org.main;
 
 import org.bangundatar.*;
