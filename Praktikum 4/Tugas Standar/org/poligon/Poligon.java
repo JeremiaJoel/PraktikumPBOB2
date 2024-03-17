12bc4bf88039b321fe6b5ad60bf31564a393dac9
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-03-17 16:58:35 +0700

    Add files via upload

---
+++ Praktikum 4/Tugas Standar/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/**
+ * File :Poligon.java 16/03/2024
+ * Penulis :Jeremia Joel Richard Ramalael
+ * Deskripsi:
+ */
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi() {
+        return this.jumlahSisi;
+    }
+
+}
