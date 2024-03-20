33787f87cd4b6a11fe6559e20b480d83731958f5
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-03-20 21:36:46 +0700

    Add files via upload

---
+++ Praktikum 5/Abstrak/BujurSangkar.java
@@ -0,0 +1,12 @@
+/**
+ * BujurSangkar.java
+ * Penulis: Jeremia Joel Richard Ramalael
+ * Deskripsi:
+ */
+
+public class BujurSangkar extends BangunDatar {
+    public double hitungLuas(double sisi){
+        luas = sisi * sisi;
+        return luas;
+    }
+}
